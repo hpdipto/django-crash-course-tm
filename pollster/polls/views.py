6bde1da89983cd6b7d816d934c74379e6765deb6
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-08-25 08:44:58 +0600

    URLs added

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 from .models import Question, Choice
 
-
+# Get question and display them
+def index(request):
+    return render(request, 'polls/index.html')
 
